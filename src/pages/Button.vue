5d2dd1a43f2df1794dd0d41711eded70d22ceeac
Oscar <oalbornoz08@gmail.com>
2020-09-24 12:46:34 -0400

    Updtae layout #2

--- src/pages/Button.vue
+++ src/pages/Button.vue
@@ -30,49 +30,54 @@ export default Component;
       <h2 class="title is-4">
         <a href="/documentation/button/#api" class="router-link-exact-active is-active">#</a> API
       </h2>
-
-      <v-tabs v-model="tab" type="is-boxed">
-        <v-tab label="Properties">
-          <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Values</th>
-                <th>Default</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(value, index) in api.props" :key="index">
-                <th><code>{{ value.name }}</code></th>
-                <td><v-tag>{{ value.type }}</v-tag></td>
-                <td>{{ value.values }}</td>
-                <td>{{ value.default }}</td>
-                <td>{{ value.description }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </v-tab>
-        <v-tab label="Events">
-          <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Parameters</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(value, index) in api.events" :key="index">
-                <th><code>{{ value.name }}</code></th>
-                <td>{{ value.description }}</td>
-                <td><v-tag>{{ value.parameters }}</v-tag></td>
-              </tr>
-            </tbody>
-          </table>
-        </v-tab>
-      </v-tabs>
+      
+      <div v-for="(item, key) in api" :key="key">
+        <p class="pb-4 is-size-4">
+          {{ item.title }}
+        </p>
+        <v-tabs v-model="tab" type="is-boxed">
+          <v-tab label="Properties">
+            <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Values</th>
+                  <th>Default</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(value, index) in item.props" :key="index">
+                  <th><code>{{ value.name }}</code></th>
+                  <td><v-tag>{{ value.type }}</v-tag></td>
+                  <td>{{ value.values }}</td>
+                  <td>{{ value.default }}</td>
+                  <td>{{ value.description }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </v-tab>
+          <v-tab label="Events">
+            <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Parameters</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(value, index) in item.events" :key="index">
+                  <th><code>{{ value.name }}</code></th>
+                  <td>{{ value.description }}</td>
+                  <td><v-tag>{{ value.parameters }}</v-tag></td>
+                </tr>
+              </tbody>
+            </table>
+          </v-tab>
+        </v-tabs>
+      </div>
     </section>
   </div>
 </template>
--- src/pages/Documentation.vue
+++ src/pages/Documentation.vue
@@ -18,68 +18,66 @@ export default Component;
 </script>
 
 <template>
-  <section>
-    <section class="hero is-fullheight has-background-white pt-6">
-      <v-columns class="pt-5 mx-0">
-        <v-column size="is-2">
-          <div class="pt-6 px-5">
-            <v-menu>
-              <v-menu-list label="Getting Started">
-                <v-menu-item label="Installation" />
-                <v-menu-item label="Why Vue3-ui" />
-                <v-menu-item label="Theming" />
-              </v-menu-list>
-              <v-menu-list label="Elements">
-                <!-- <v-menu-item label="Typography" /> -->
-                <!-- <v-menu-item label="Box" /> -->
-                <v-menu-item tag="router-link" :to="{name: 'button'}" label="Button" />
-                <!-- <v-menu-item label="Content" /> -->
-                <!-- <v-menu-item label="Delete" /> -->
-                <!-- <v-menu-item label="Form" /> -->
-                <!-- <v-menu-item label="Icon" /> -->
-                <v-menu-item label="Image" />
-                <!-- <v-menu-item label="Notifications" /> -->
-                <v-menu-item label="Progress" />
-                <!-- <v-menu-item label="Table" /> -->
-                <v-menu-item label="Tag" />
-              </v-menu-list>
-              <v-menu-list label="Form controls">
-                <v-menu-item label="Input" />
-                <v-menu-item label="Switch" />
-                <v-menu-item label="Select" />
-                <v-menu-item label="Textarea" />
-                <v-menu-item label="Checkbox" />
-              </v-menu-list>
-              <v-menu-list label="Components">
-                <!-- <v-menu-item label="Breadcrumb" /> -->
-                <v-menu-item label="Dropdown" />
-                <v-menu-item label="Card" />
-                <!-- <v-menu-item label="Hero" /> -->
-                <!-- <v-menu-item label="Level" /> -->
-                <!-- <v-menu-item label="Media" /> -->
-                <v-menu-item label="Menu" />
-                <!-- <v-menu-item label="Message" /> -->
-                <v-menu-item label="Modal" />
-                <v-menu-item label="Navbar" />
-                <!-- <v-menu-item label="Pagination" /> -->
-                <!-- <v-menu-item label="Panel" /> -->
-                <v-menu-item label="Tabs" />
-              </v-menu-list>
-            </v-menu>
-          </div>
-        </v-column>
-        <v-column class="pt-6">
-          <v-breadcrumb>
-            <v-breadcrumb-item tag="router-link" :to="{ name: 'home' }">
-              Home
-            </v-breadcrumb-item>
-            <v-breadcrumb-item v-for="item in path.split('/').slice(1)" :key="item" class="is-capitalized" tag="router-link" :to="{ name: item }">
-              {{ item }}
-            </v-breadcrumb-item>
-          </v-breadcrumb>
-          <router-view />
-        </v-column>
-      </v-columns>
-    </section>
+  <section class="has-background-white pt-6">
+    <v-columns class="pt-5 mx-0">
+      <v-column size="is-2">
+        <div class="pt-6 px-5">
+          <v-menu>
+            <v-menu-list label="Getting Started">
+              <v-menu-item label="Installation" />
+              <v-menu-item label="Why Vue3-ui" />
+              <v-menu-item label="Theming" />
+            </v-menu-list>
+            <v-menu-list label="Elements">
+              <!-- <v-menu-item label="Typography" /> -->
+              <!-- <v-menu-item label="Box" /> -->
+              <v-menu-item tag="router-link" :to="{name: 'button'}" label="Button" />
+              <!-- <v-menu-item label="Content" /> -->
+              <!-- <v-menu-item label="Delete" /> -->
+              <!-- <v-menu-item label="Form" /> -->
+              <!-- <v-menu-item label="Icon" /> -->
+              <v-menu-item label="Image" />
+              <!-- <v-menu-item label="Notifications" /> -->
+              <v-menu-item label="Progress" />
+              <!-- <v-menu-item label="Table" /> -->
+              <v-menu-item label="Tag" />
+            </v-menu-list>
+            <v-menu-list label="Form controls">
+              <v-menu-item label="Input" />
+              <v-menu-item label="Switch" />
+              <v-menu-item label="Select" />
+              <v-menu-item label="Textarea" />
+              <v-menu-item label="Checkbox" />
+            </v-menu-list>
+            <v-menu-list label="Components">
+              <!-- <v-menu-item label="Breadcrumb" /> -->
+              <v-menu-item label="Dropdown" />
+              <v-menu-item label="Card" />
+              <!-- <v-menu-item label="Hero" /> -->
+              <!-- <v-menu-item label="Level" /> -->
+              <!-- <v-menu-item label="Media" /> -->
+              <v-menu-item label="Menu" />
+              <!-- <v-menu-item label="Message" /> -->
+              <v-menu-item label="Modal" />
+              <v-menu-item label="Navbar" />
+              <!-- <v-menu-item label="Pagination" /> -->
+              <!-- <v-menu-item label="Panel" /> -->
+              <v-menu-item label="Tabs" />
+            </v-menu-list>
+          </v-menu>
+        </div>
+      </v-column>
+      <v-column class="pt-6">
+        <v-breadcrumb>
+          <v-breadcrumb-item tag="router-link" :to="{ name: 'home' }">
+            Home
+          </v-breadcrumb-item>
+          <v-breadcrumb-item v-for="item in path.split('/').slice(1)" :key="item" class="is-capitalized" tag="router-link" :to="{ name: item }">
+            {{ item }}
+          </v-breadcrumb-item>
+        </v-breadcrumb>
+        <router-view />
+      </v-column>
+    </v-columns>
   </section>
 </template>
