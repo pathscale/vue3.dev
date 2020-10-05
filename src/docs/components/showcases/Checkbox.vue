241c587eed60eb92812763990400789312838e36
Oscar <oalbornoz08@gmail.com>
2020-10-05 16:14:24 -0400

    Add checkbox showcase

---
+++ src/docs/components/showcases/Checkbox.vue
@@ -0,0 +1,37 @@
+<script>
+import { VField, VCheckbox } from '@pathscale/vue3-ui'
+import { ref } from "vue"
+
+const Component = {
+  components: { VField, VCheckbox },
+  setup() {
+    const value = ref(false);
+    return { value }
+  }
+}
+export default Component;
+</script>
+<template>
+  <section>
+    <v-field>
+      <v-checkbox v-model="value">
+        Active checkbox
+      </v-checkbox>
+      <br />
+      <v-checkbox indeterminate>
+        Indeterminate checkbox
+      </v-checkbox>
+      <br />
+      <v-checkbox disabled>
+        Disabled checkbox
+      </v-checkbox>
+    </v-field>
+    
+
+    <v-field :message="value" label="Custom values">
+      <v-checkbox v-model="value" true-value="YES" false-value="NO">
+        Accept?
+      </v-checkbox>
+    </v-field>
+  </section>
+</template>
