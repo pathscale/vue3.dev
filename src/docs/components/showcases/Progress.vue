45fc43b28c4bd7ddc87de1329ed35e8c45ea4a9d
Oscar <oalbornoz08@gmail.com>
2020-10-05 13:40:11 -0400

    Clean Progress showcase code

--- src/docs/components/showcases/Progress.vue
+++ src/docs/components/showcases/Progress.vue
@@ -13,35 +13,30 @@ export default Component;
     </v-field>
     <v-field label="Colors">
       <v-progress type="is-danger" :value="40" />
-    </v-field>
-    <v-field>
       <v-progress type="is-success" :value="60" />
-    </v-field>
-    <v-field>
       <v-progress type="is-info" :value="80" />
-    </v-field>
-    <v-field>
       <v-progress type="is-warning" :value="100" />
     </v-field>
 
     <v-field label="Sizes"> 
       <v-progress size="is-small" value="20" />
-    </v-field>
-    <v-field>
       <v-progress value="30" />
-    </v-field>
-    <v-field>
       <v-progress size="is-medium" value="40" />
-    </v-field>
-    <v-field>
       <v-progress size="is-large" value="50" />
     </v-field>
 
     <v-field label="Show value">
       <v-progress :value="40" show-value />
     </v-field>
+    
     <v-field label="Percent">
       <v-progress :value="80" show-value format="percent" />
     </v-field>
   </section>
 </template>
+
+<style scoped>
+.progress-wrapper {
+    margin-top: 1em;
+}
+</style>
