edea837e0f25aa1ad9ce75d2080c6251ccaeb1f6
Oscar <oalbornoz08@gmail.com>
2020-10-06 18:44:38 -0400

    Add Navbar showcase

---
+++ src/docs/components/showcases/Navbar.vue
@@ -0,0 +1,51 @@
+<script>
+import { VNavbar, VNavbarItem, VNavbarDropdown, VButton } from '@pathscale/vue3-ui'
+
+const Component = {
+  components: { VNavbar, VNavbarItem, VNavbarDropdown, VButton },
+}
+export default Component;
+</script>
+<template>
+  <section>
+    <v-navbar type="is-info">
+      <template #brand>
+        <v-navbar-item tag="router-link" :to="{ name: 'navbar' }">
+          <img width="100" height="100" class="has-background-grey-light" />
+        </v-navbar-item>
+      </template>
+      <template #start>
+        <v-navbar-item href="#" active>
+          Home
+        </v-navbar-item>
+        <v-navbar-item href="#">
+          Documentation
+        </v-navbar-item>
+        <v-navbar-dropdown label="Info" hoverable>
+          <v-navbar-item href="#" active>
+            About
+          </v-navbar-item>
+          <v-navbar-item href="#">
+            Contact
+          </v-navbar-item>
+        </v-navbar-dropdown>
+      </template>
+
+      <template #end>
+        <v-navbar-item tag="div">
+          <div class="buttons">
+            <v-button type="is-success">
+              <strong>Sign up</strong>
+            </v-button>
+            <v-button type="is-light">
+              Log in
+            </v-button>
+          </div>
+        </v-navbar-item>
+      </template>
+    </v-navbar>
+  </section>
+</template>
+
+<style scoped>
+</style>
