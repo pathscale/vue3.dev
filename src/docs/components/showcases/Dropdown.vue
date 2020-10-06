46e2e42c2d16b1997061a9e424964bc6ea24053e
David <david.cedresx@gmail.com>
2020-10-06 16:36:27 -0400

    add further dropdown examples

--- src/docs/components/showcases/Dropdown.vue
+++ src/docs/components/showcases/Dropdown.vue
@@ -13,7 +13,7 @@ export default Component
 </script>
 <template>
   <section>
-    <h4 class="is-size-5 mb-4">Dropdown</h4>
+    <h4 class="is-size-5 mb-4">Basic Dropdown</h4>
     <v-dropdown v-model="item">
       <template #trigger>
         <button class="button is-primary">
@@ -26,5 +26,93 @@ export default Component
       <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
       <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
     </v-dropdown>
+
+    <h4 class="is-size-5 mb-4 mt-6">Hoverable Dropdown</h4>
+    <v-dropdown v-model="item" hoverable>
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu">Pikachu</v-dropdown-item>
+      <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
+      <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
+    </v-dropdown>
+
+
+    <h4 class="is-size-5 mb-4 mt-6">Disabled Dropdown</h4>
+    <v-dropdown v-model="item" disabled>
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu">Pikachu</v-dropdown-item>
+      <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
+      <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
+    </v-dropdown>
+
+    <h4 class="is-size-5 mb-4 mt-6">Left dropdown</h4>
+    <v-dropdown v-model="item" position="is-left">
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu">Pikachu</v-dropdown-item>
+      <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
+      <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
+    </v-dropdown>
+
+    <h4 class="is-size-5 mb-4 mt-6">Right dropdown</h4>
+    <v-dropdown v-model="item" position="is-right">
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu">Pikachu</v-dropdown-item>
+      <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
+      <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
+    </v-dropdown>
+
+    <h4 class="is-size-5 mb-4 mt-6">Up dropdown</h4>
+    <v-dropdown v-model="item" position="is-up">
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu">Pikachu</v-dropdown-item>
+      <v-dropdown-item value="Charmander">Charmander</v-dropdown-item>
+      <v-dropdown-item value="Squirtle">Squirtle</v-dropdown-item>
+    </v-dropdown>
+
+
+    <h4 class="is-size-5 mb-4 mt-6">Dropdown with link</h4>
+    <v-dropdown>
+      <template #trigger>
+        <button class="button is-primary">
+          <span v-if="!item">Choose your pokemon</span>
+          <span v-else>I choose you, {{ item}} </span>
+        </button>
+      </template>
+
+      <v-dropdown-item value="Pikachu" has-link>
+        <a href="https://google.com" target="_blank" has-link>This is a link</a>
+      </v-dropdown-item>
+    </v-dropdown>
+
+    
   </section>
 </template>
