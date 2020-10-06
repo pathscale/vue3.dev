716176e65806af1cfa4fc2d804395e3f645f971a
Oscar <oalbornoz08@gmail.com>
2020-10-05 23:23:25 -0400

    Add Card showcase

---
+++ src/docs/components/showcases/Card.vue
@@ -0,0 +1,41 @@
+<script>
+import { VCard, VCardImage, VCardContent, VImage, VMedia, VColumns, VColumn } from '@pathscale/vue3-ui'
+
+const Component = {
+  components: { VCard, VCardImage, VCardContent, VImage, VMedia, VColumns, VColumn },
+}
+export default Component;
+</script>
+<template>
+  <section>
+    <v-columns>
+      <v-column size="is-3">
+        <v-card>
+          <v-image src="https://vue3--dev.b-cdn.net/128x128.png" alt="placeholder image" />
+          <v-card-content>
+            <v-media>
+              <template #left>
+                <v-image src="https://vue3--dev.b-cdn.net/128x128.png" size="is-48x48" rounded alt="placeholder image" />
+              </template>
+              <template #content>
+                <p class="title is-4">
+                  John Smith
+                </p>
+                <p class="subtitle is-6">
+                  @johnsmith
+                </p>
+              </template>
+            </v-media>
+          </v-card-content>
+          <div class="content px-4 py-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris. <a>@vue3-ui</a>.
+            <a href="#">#vue3</a> <a href="#">#responsive</a>
+            <br />
+            <time datetime="2020-5-8">11:22 PM - 5 Oct 2020</time>
+          </div>
+        </v-card>
+      </v-column>
+    </v-columns>
+  </section>
+</template>
