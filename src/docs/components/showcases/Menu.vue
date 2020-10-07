4ed60c73e3907d0b7b024f5fc060d559aee4a7d5
Oscar <oalbornoz08@gmail.com>
2020-10-07 10:40:16 -0400

    Add menu showcase

---
+++ src/docs/components/showcases/Menu.vue
@@ -0,0 +1,32 @@
+<script>
+import { VMenu, VMenuItem, VMenuList } from '@pathscale/vue3-ui'
+
+const Component = {
+  components: { VMenu, VMenuItem, VMenuList },
+}
+export default Component;
+</script>
+<template>
+  <section>
+    <v-menu>
+      <v-menu-list label="Menu">
+        <v-menu-item label="Info" />
+        <v-menu-item :active="true" expanded label="Administrator">
+          <v-menu-item label="Users" />
+          <v-menu-item label="Devices" />
+          <v-menu-item label="Payments" disabled />
+        </v-menu-item>
+        <v-menu-item label="My Account">
+          <v-menu-item label="Account data" />
+          <v-menu-item label="Addresses" />
+        </v-menu-item>
+      </v-menu-list>
+      <v-menu-list>
+        <v-menu-item label="Expo" tag="router-link" target="_blank" to="/" />
+      </v-menu-list>
+      <v-menu-list label="Actions">
+        <v-menu-item label="Logout" />
+      </v-menu-list>
+    </v-menu>
+  </section>
+</template>
--- src/pages/components/Menu.vue
+++ src/pages/components/Menu.vue
@@ -3,14 +3,17 @@ import { useI18n } from "vue-composable";
 
 import { useRouter } from 'vue-router';
 import api from '../../docs/components/api/menu.ts'
-import { ApiSection } from "../../components/sections"
+import { SnippetSection, ApiSection } from "../../components"
+
+import Showcase from "../../docs/components/showcases/Menu.vue"
+import ShowcaseCode from "../../docs/components/raw/Menu.txt"
 
 const Component = {
-  components: { ApiSection },
+  components: { SnippetSection, ApiSection },
   setup() {
     const intl = useI18n();
      const router = useRouter();
-    return { intl, api, router }
+    return { intl, api, router, Showcase, ShowcaseCode }
   }
 }
 
@@ -25,6 +28,7 @@ export default Component;
     <p>
       {{ router.currentRoute.value.meta.metaTags[0].content }}
     </p>
+    <snippet-section :code="ShowcaseCode" :component="Showcase" :path="router.currentRoute.value.path" />
     <section id="api" class="pt-4">
       <h2 class="title is-4">
         <a :href="`${router.currentRoute.value.path}#api`" class="is-active">#</a> API
