cd11cd31d1fce72e15f8e5f03ee70ddf9a45ac94
Oscar <oalbornoz08@gmail.com>
2020-10-08 10:01:49 -0400

    Add Sidebar showcase

---
+++ src/docs/components/showcases/Sidebar.vue
@@ -0,0 +1,51 @@
+<script>
+import { VSidebar, VButton, VSwitch, VField } from "@pathscale/vue3-ui"
+import { reactive } from 'vue';
+
+const Component = {
+  components: { VSidebar, VButton, VSwitch, VField },
+  setup(props) {
+    const state = reactive({
+      state: true,
+      overlay: false,
+      reduced: false,
+    })
+
+    function close() {
+      state.open = false
+    }
+
+    return { state, close }
+  }
+}
+
+export default Component;
+</script>
+
+<template>
+  <v-field>
+    <v-switch v-model="state.open" type="is-dark">
+      Open
+    </v-switch>
+    <v-switch v-model="state.overlay" type="is-dark">
+      Overlay
+    </v-switch>
+    <v-switch v-model="state.reduced" type="is-dark">
+      Reduced
+    </v-switch>
+  </v-field>
+  
+  <v-sidebar :open="state.open" type="is-light" fullheight :reduce="state.reduced" :overlay="state.overlay" @close="close()" position="fixed">
+    <section class="section px-1">
+      <v-button tag="a" @click="close()" type="is-text" class="is-pulled-right">
+        &#x274C;
+      </v-button>
+    </section>
+  </v-sidebar>
+</template>
+<style>
+.button.is-text {
+    text-decoration: none;
+    outline: none;
+}
+</style>
