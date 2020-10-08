491842a4fcca574e41976bf2713dbb861be0eada
Oscar <oalbornoz08@gmail.com>
2020-10-08 10:02:02 -0400

    Add Sidebar showcase

--- src/pages/components/Sidebar.vue
+++ src/pages/components/Sidebar.vue
@@ -3,14 +3,18 @@ import { useI18n } from "vue-composable";
 
 import { useRouter } from 'vue-router';
 import api from '../../docs/components/api/sidebar.ts'
-import { ApiSection } from "../../components/sections"
+import { SnippetSection, ApiSection } from "../../components"
+
+import Showcase from "../../docs/components/showcases/Sidebar.vue"
+import ShowcaseCode from "../../docs/components/raw/Sidebar.txt"
 
 const Component = {
-  components: { ApiSection },
+  components: { SnippetSection, ApiSection },
   setup() {
     const intl = useI18n();
-     const router = useRouter();
-    return { intl, api, router }
+    const router = useRouter();
+    const path = `${router.currentRoute.value.path}#api`;
+    return { intl, api, router, path, Showcase, ShowcaseCode }
   }
 }
 
@@ -25,9 +29,10 @@ export default Component;
     <p>
       {{ router.currentRoute.value.meta.metaTags[0].content }}
     </p>
+    <snippet-section :code="ShowcaseCode" :component="Showcase" :path="router.currentRoute.value.path" />
     <section id="api" class="pt-4">
       <h2 class="title is-4">
-        <a :href="`${router.currentRoute.value.path}#api`" class="is-active">#</a> API
+        <a :href="path" class="is-active">#</a> API
       </h2>
       <api-section :api="api" />
     </section>
