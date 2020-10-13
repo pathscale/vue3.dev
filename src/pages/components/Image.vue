65881e7063a80d13a420ea6bdf8a87688f49c42f
Oscar <oalbornoz08@gmail.com>
2020-10-13 01:32:54 -0400

    Add image variables

--- src/pages/components/Image.vue
+++ src/pages/components/Image.vue
@@ -2,20 +2,20 @@
 import { useI18n } from 'vue-composable'
 
 import { useRouter } from 'vue-router'
-import { SnippetSection, ApiSection } from '../../components'
+import { SnippetSection, ApiSection, VariablesSection } from '../../components'
 
 import api from '../../docs/components/api/image.ts'
-
+import variables from '../../docs/components/variables/image.json'
 import Showcase from '../../docs/components/showcases/Image.vue'
 import ShowcaseCode from '../../docs/components/raw/Image.txt'
 
 export default {
   name: 'DevPageImage',
-  components: { SnippetSection, ApiSection },
+  components: { SnippetSection, ApiSection, VariablesSection },
   setup() {
     const intl = useI18n()
     const router = useRouter()
-    return { intl, api, router, Showcase, ShowcaseCode }
+    return { intl, api, variables, router, Showcase, ShowcaseCode }
   }
 }
 </script>
@@ -34,6 +34,12 @@ export default {
         <a :href="`${router.currentRoute.value.path}#api`" class="is-active">#</a> API
       </h2>
       <api-section :api="api" />
+    </section>
+    <section id="variables" class="pt-4">
+      <h2 class="title is-4">
+        <a :href="`${router.currentRoute.value.path}#variables`" class="is-active">#</a> Variables
+      </h2>
+      <variables-section :variables="variables" />
     </section>
   </div>
 </template>
