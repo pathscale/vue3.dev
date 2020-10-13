0d18fcb8f4bae6d4385c79ae392a879ddaaf0f0c
Oscar <oalbornoz08@gmail.com>
2020-10-13 00:35:30 -0400

    Add button variables

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Cookie } from './footer/Cookie.vue'
 
 export { default as SnippetSection } from './sections/Snippet.vue'
 export { default as ApiSection } from './sections/Api.vue'
+export { default as VariablesSection } from './sections/Variables.vue'
---
+++ src/components/sections/Variables.vue
@@ -0,0 +1,34 @@
+<script>
+import { VTag } from '@pathscale/vue3-ui'
+
+export default {
+  name: 'DevVariables',
+  components: { VTag },
+  props: {
+    'variables': Object
+  }
+}
+</script>
+
+<template>
+  <div>
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Default value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(name, index) in Object.keys(variables)" :key="index">
+          <td><code>{{ name }}</code></td>
+          <td>
+            <v-tag>
+              {{ variables[name] || '—' }}
+            </v-tag>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
