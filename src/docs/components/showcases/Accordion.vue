9a0c98781c60383c89a6c9e2d8c45ef09d535909
radupasparuga <radupasparuga25@gmail.com>
2020-10-16 20:29:52 +0300

    Temp fix accordion

--- src/docs/components/showcases/Accordion.vue
+++ src/docs/components/showcases/Accordion.vue
@@ -1,147 +1,24 @@
 <script>
-import { VAccordion, VButton, VField, VTag } from '@pathscale/vue3-ui'
+import { VAccordion, VField } from '@pathscale/vue3-ui'
 
 export default {
   name: 'DevShowcaseAccordion',
-  components: { VAccordion, VButton, VField, VTag }
+  components: { VAccordion, VField }
 }
 </script>
 
 <template>
   <v-field>
-    <v-accordion>
+    <v-accordion header-is-trigger background="#d9dedb">
       <template #header>
-        <v-button type="is-success" class="my-2">
-          Basic accordion
-        </v-button>
+        <div class="py-3 px-3">
+          Click me
+        </div>
       </template>
       <template #content>
-        Content bro
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion is-horizontal>
-      <template #header>
-        <v-button type="is-info" class="my-2">
-          Horizontal accordion
-        </v-button>
-      </template>
-      <template #content>
-        Greetings
-      </template>
-    </v-accordion>
-  </v-field>
-  <v-field>
-    <v-accordion header-is-trigger trigger-left>
-      <template #trigger>
-        <v-button type="is-warning" class="my-2">
-          Accordion with left trigger
-        </v-button>
-      </template>
-      <template #content>
-        Hello
-      </template>
-    </v-accordion>
-  </v-field>
-  <v-field>
-    <v-accordion header-is-trigger trigger-right>
-      <template #trigger>
-        <v-button type="is-danger" class="my-2">
-          Accordion with right trigger
-        </v-button>
-      </template>
-      <template #content>
-        Hello
-      </template>
-    </v-accordion>
-  </v-field>
-  <v-field>
-    <v-accordion header-is-trigger expanded>
-      <template #trigger>
-        <v-button type="is-danger" class="my-2">
-          Expanded Accordion
-        </v-button>
-      </template>
-      <template #content>
-        Hello
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger background="var(--blm-sucs)">
-      <template #trigger>
-        <v-button type="is-link" class="my-2">
-          Accordion with custom background
-        </v-button>
-      </template>
-      <template #content>
-        content
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger color="var(--blm-dang)">
-      <template #trigger>
-        <v-button type="is-link" class="my-2">
-          Accordion with custom color
-        </v-button>
-      </template>
-      <template #content>
-        Content
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger hover>
-      <template #trigger>
-        <v-button type="is-info" class="my-2">
-          Hover
-        </v-button>
-      </template>
-      <template #content>
-        Content
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger is-link>
-      <template #trigger>
-        <v-button type="is-info" class="my-2">
-          Accordion with link
-        </v-button>
-      </template>
-      <template #content>
-        <a href="https://github.com/pathscale/vue3-ui" target="_blank" rel="noopener">Go to source</a>
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger is-active>
-      <template #trigger>
-        <v-button type="is-info" class="my-2">
-          Active accordion
-        </v-button>
-      </template>
-      <template #content>
-        Content
-      </template>
-    </v-accordion>
-  </v-field>
-
-  <v-field>
-    <v-accordion header-is-trigger>
-      <template #header>
-        <v-tag>Accordion with header as trigger</v-tag>
-      </template>
-      <template #content>
-        Content
+        <div class="py-3 px-3">
+          Content
+        </div>
       </template>
     </v-accordion>
   </v-field>
