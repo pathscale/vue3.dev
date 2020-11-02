3d1344d5068cd0e1ba37a82f307c2f3887312f40
David <david.cedresx@gmail.com>
2020-11-02 04:56:18 -0400

    update packages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@pathscale/bulma-pull-2981-css-var-only/css/bulma.css'
-import '@pathscale/bulma-extensions-css-var'
-import 'vue3-carousel/dist/carousel.css'
 
+import '@pathscale/bulma-pull-2981-css-var-only'
+import '@pathscale/bulma-extensions-css-var'
+
+import 'vue3-carousel/dist/carousel.css'
 import './assets/styles/index.css'
 // import "./assets/styles/scrollbar.css";
 import './assets/styles/carousel.css'
