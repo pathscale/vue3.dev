8940f1431b0c5fa740dc3f3be79a41fd2c72bfa0
Oscar <oalbornoz08@gmail.com>
2020-11-05 13:57:32 -0400

    Add Overview text

--- src/components/sections/Trailer.vue
+++ src/components/sections/Trailer.vue
@@ -4,12 +4,23 @@ import { useI18n } from 'vue-composable'
 
 import 'vue3-carousel/dist/carousel.css'
 
+import { useRouter } from 'vue-router'
+
+
 export default {
   name: 'DevTrailer',
   components: { VButton, VColumns, VColumn },
   setup() {
     const intl = useI18n()
-    return { intl }
+    const router = useRouter()
+
+    function redirect(name) {
+      document.querySelector('#well').scrollIntoView() // Scroll to top
+      router.push({
+        name
+      })
+    }
+    return { intl, redirect }
   }
 }
 </script>
@@ -24,11 +35,18 @@ export default {
         <v-columns hcentered>
           <v-column size="is-6" class="has-text-centered">
             <p class="has-text-white is-size-5 is-size-6-touch">
-              {{ intl.$ts('trailer.content') }}
+              Vue3-ui provides one of the most complete implementations of the <a href="https://bulma.io/" target="_blank" rel="noopener">Bulma</a> components and grid system, following robust guidelines to do those really easy and fast to use.
+              <br /><br />
+              Based in <a href="https://v3.vuejs.org/" target="_blank" rel="noopener">Vue 3</a>, our source code is optimized to take full advantage of <a href="https://composition-api.vuejs.org/" target="_blank" rel="noopener">Composition API</a>, offering better performance and reduced javascript load.
             </p>
-            <v-button type="is-primary" rounded inverted outlined class="mt-6 mb-6 is-uppercase">
-              {{ intl.$ts('trailer.button') }}
-            </v-button>
+            <div class="pt-3">
+              <v-button type="is-primary" rounded inverted outlined class="my-6 mx-5 is-uppercase" @click="redirect('installation')">
+                Getting Started
+              </v-button>
+              <v-button type="is-primary" rounded inverted outlined class="my-6 mx-5 is-uppercase" tag="a" href="https://github.com/pathscale/vue3-ui" target="_blank" rel="noopener">
+                Github
+              </v-button>
+            </div>
           </v-column>
         </v-columns>
       </div>
