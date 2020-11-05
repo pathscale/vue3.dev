8fd2a8c2d09641b8b413d7f99d5c06cf92cd2c45
Oscar <oalbornoz08@gmail.com>
2020-11-05 19:24:43 -0400

    Update client images

--- src/components/sections/Clients.vue
+++ src/components/sections/Clients.vue
@@ -1,38 +1,29 @@
 <script>
-import { VButton, VColumns, VColumn } from '@pathscale/vue3-ui'
+import { VColumns, VColumn } from '@pathscale/vue3-ui'
 import { useI18n } from 'vue-composable'
-import { ref } from 'vue'
 
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
   name: 'DevClients',
-  components: { VButton, VColumns, VColumn, Carousel, Slide, Pagination, Navigation },
+  components: { VColumns, VColumn },
   setup() {
     const intl = useI18n()
-    const isLandscape = ref(window.matchMedia('(orientation: landscape)').matches)
-    window.addEventListener('orientationchange', () => {
-      isLandscape.value = !isLandscape.value
-    })
+    const url = 'https://vue3--dev.b-cdn.net'
+
 
     const projects = [
       {
-        title: 'Space Revenge',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ornare mi. Vestibulum finibus, mi iaculis vulputate vehicula, nisl nunc venenatis nisl, id fringilla turpis diam eget dolor. Cras lobortis lectus nibh, eu facilisis sapien suscipit sed',
-        image: 'https://spacerevenge--game.b-cdn.net/battle.webp',
-        link: 'https://www.revenge.game/',
-        button: 'Spacerevenge.game'
+        title: 'Pathscale.com',
+        image: `${url}/pathscale.png`,
+        link: 'https://www.pathscale.com/'
       },
       {
-        title: 'Social.soy',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at ornare mi. Vestibulum finibus, mi iaculis vulputate vehicula, nisl nunc venenatis nisl, id fringilla turpis diam eget dolor. Cras lobortis lectus nibh, eu facilisis sapien suscipit sed',
-        image: 'https://spacerevenge--game.b-cdn.net/chat.webp',
-        link: 'https://social.soy/',
-        button: 'Social.soy'
+        title: 'Vue3.dev',
+        image: `${url}/vue3-dev.png`,
+        link: 'https://vue3.dev/'
       }
     ]
-    return { intl, isLandscape, projects }
+    return { intl, projects }
   }
 }
 </script>
@@ -44,49 +35,11 @@ export default {
         <p class="title has-text-white has-text-centered">
           {{ intl.$ts('clients.title') }}
         </p>
-        <carousel>
-          <slide v-for="(client, index) in projects" :key="index" :items-to-show="1.5" wrap-around initial-slide="1">
-            <v-columns :mobile="isLandscape" vcentered class="reverse-columns px-2">
-              <v-column class="has-text-centered" style="width: 100%;">
-                <img :src="client.image" :ref="client.title" />
-                <v-button
-                  tag="a"
-                  :href="client.link"
-                  target="_blank"
-                  rel="noopener"
-                  class="mt-6 is-size-6 is-size-7-touch is-hidden-desktop is-hidden-fullhd"
-                  rounded
-                  type="is-white"
-                  size="is-medium">
-                  {{ client.button }}
-                </v-button>
-              </v-column>
-              <v-column size="is-full-touch is-half">
-                <p class="subtitle has-text-white">
-                  {{ client.title }}
-                </p>
-                <p class="has-text-white">
-                  {{ client.description }}
-                </p>
-                <v-button
-                  tag="a"
-                  :href="client.link"
-                  target="_blank"
-                  rel="noopener"
-                  class="mt-6 is-size-6 is-size-7-touch is-hidden-touch"
-                  rounded
-                  type="is-white"
-                  size="is-medium">
-                  {{ client.button }}
-                </v-button>
-              </v-column>
-            </v-columns>
-          </slide>
-          <template #addons>
-            <navigation />
-            <pagination />
-          </template>
-        </carousel>
+        <v-columns vcentered class="reverse-columns px-2" gap="is-6">
+          <v-column v-for="(client, index) in projects" :key="index" class="has-text-centered mx-6">
+            <a :href="client.link"><img :src="client.image" :ref="client.title" /></a>
+          </v-column>
+        </v-columns>
       </div>
     </div>
   </section>
