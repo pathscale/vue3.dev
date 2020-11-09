669750d1a6478e3b798ae35dd65fc57a4baab09d
Oscar <oalbornoz08@gmail.com>
2020-11-09 13:28:00 -0400

    Minor change

--- src/docs/components/showcases/Image.vue
+++ src/docs/components/showcases/Image.vue
@@ -9,4 +9,7 @@ export default {
   <img src="#" data-src="./128x128.png" alt="src # and data-src" />
   <img src="./128x128.png" alt="only src" />
   <img src="./128x128.png" data-src="./128x128.png" alt="src and data-src" />
+  <v-image src="https://vue3--dev.b-cdn.net/128x128.png"
+           alt="placeholder image"
+           size="is-128x128" rounded centered />
 </template>
