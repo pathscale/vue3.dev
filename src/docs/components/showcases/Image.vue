3e47990c1bc49ebc91d72c0ac358e38f65478ecf
Oscar <oalbornoz08@gmail.com>
2020-11-09 16:41:05 -0400

    Clean image showcase

--- src/docs/components/showcases/Image.vue
+++ src/docs/components/showcases/Image.vue
@@ -22,7 +22,4 @@ export default {
     Relative path
   </p>
   <img data-src="./128x128.png" alt="1 only data-src" />
-  <img src="#" data-src="./128x128.png" alt="1 src # and data-src" />
-  <img src="./128x128.png" alt="3 only src" />
-  <img src="./128x128.png" data-src="./128x128.png" alt="4 src and data-src" />
 </template>
