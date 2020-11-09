0427df87d2f6d45d0268238cd520f88df0b8b97f
Oscar <oalbornoz08@gmail.com>
2020-11-09 13:16:14 -0400

    Add temporal benchie test

--- src/docs/components/showcases/Image.vue
+++ src/docs/components/showcases/Image.vue
@@ -1,14 +1,12 @@
 <script>
-import { VImage } from '@pathscale/vue3-ui'
-
 export default {
-  name: 'DevShowcaseImage',
-  components: { VImage }
+  name: 'DevShowcaseImage'
 }
 </script>
 
 <template>
-  <v-image src="https://vue3--dev.b-cdn.net/128x128.png"
-           alt="placeholder image"
-           size="is-128x128" rounded centered />
+  <img data-src="./128x128.png" alt="only data-src" />
+  <img src="#" data-src="./128x128.png" alt="src # and data-src" />
+  <img src="./128x128.png" alt="only src" />
+  <img src="./128x128.png" data-src="./128x128.png" alt="src and data-src" />
 </template>
