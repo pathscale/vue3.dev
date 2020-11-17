dabd92a1f0f1bcd3cac47cd0adbba7b8acb0c0e3
Oscar <oalbornoz08@gmail.com>
2020-11-17 18:26:39 -0400

    Simulate app state simple test in Introducing.vue

--- src/docs/components/showcases/Appstate/Introducing.vue
+++ src/docs/components/showcases/Appstate/Introducing.vue
@@ -1,36 +1,40 @@
 <script>
-import { createState, useState } from '@pathscale/appstate-fast'
 import { VButton } from '@pathscale/vue3-ui'
-
-const globalState = createState(0)
-
-function resetCounter() {
-  globalState.value = 0
-}
+import { computed, ref } from 'vue'
+// import { createState, useState } from '@pathscale/appstate-fast' // It does not work
+import { useState } from '../../../../appstate-fast' // Works
 
 export default {
   name: 'AppstateShowcase',
   components: { VButton },
   setup() {
+    const state = useState({ count: 0 });
+    const count = computed(() => {
+      // eslint-disable-next-line no-console -- ignore
+      console.log('I must to be fire on count changes, but I do not')
+      return state.count.value
+    });
 
-    const counterState = useState(globalState)
+    const countMirror = ref(0);
 
     const incrementCounter = () => {
-      counterState.value += 1
+      state.count.set(state.count.value + 1);
+      countMirror.value = state.count.value
     }
-
     const decrementCounter = () => {
-      counterState.value += 1
+      state.count.set(state.count.value - 1);
+      countMirror.value = state.count.value
     }
-
-    return { counterState, incrementCounter, decrementCounter, resetCounter }
+    return { incrementCounter, decrementCounter, count, countMirror }
   }
 }
 </script>
 
 <template>
+  <span class="is-size-3">
+    Counter: {{ count }} <u class="is-size-7 has-text-grey-light">(It should change just like the one below)<u /></u></span>
   <p class="is-size-3">
-    Counter: {{ counterState }}
+    Counter Ref: {{ countMirror }}
   </p>
   <div class="buttons mt-3">
     <v-button type="is-danger" @click="decrementCounter">
@@ -38,9 +42,6 @@ export default {
     </v-button>
     <v-button type="is-success" @click="incrementCounter">
       +
-    </v-button>
-    <v-button type="is-info" @click="resetCounter">
-      Reset
     </v-button>
   </div>
 </template>
