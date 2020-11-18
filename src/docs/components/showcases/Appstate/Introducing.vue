ef9c6930b2ad0a42dfe081b59f3a2204ead1c2f0
Oscar <oalbornoz08@gmail.com>
2020-11-17 21:57:11 -0400

    Bump appstate-fast package version

--- src/docs/components/showcases/Appstate/Introducing.vue
+++ src/docs/components/showcases/Appstate/Introducing.vue
@@ -1,40 +1,43 @@
 <script>
 import { VButton } from '@pathscale/vue3-ui'
-import { computed, ref } from 'vue'
-// import { createState, useState } from '@pathscale/appstate-fast' // It does not work
-import { useState } from '../../../../appstate-fast' // Works
+import { watchEffect } from 'vue'
+// eslint-disable-next-line import/no-unresolved -- ignore
+import { useState, createState } from '@pathscale/appstate-fast'
+
+const globalState = createState({ count: 0, memory: 0 })
 
 export default {
   name: 'AppstateShowcase',
   components: { VButton },
   setup() {
-    const state = useState({ count: 0 });
-    const count = computed(() => {
+    const state = useState(globalState);
+    watchEffect(() => {
       // eslint-disable-next-line no-console -- ignore
-      console.log('I must to be fire on count changes, but I do not')
-      return state.count.value
-    });
+      console.log('Memory', state.memory.value)
+    })
 
-    const countMirror = ref(0);
+    const resetCounter = () => {
+      state.memory.set(state.count.value);
+      state.count.set(0);
+    }
 
     const incrementCounter = () => {
-      state.count.set(state.count.value + 1);
-      countMirror.value = state.count.value
+      state.count.set(p => p + 1);
     }
     const decrementCounter = () => {
-      state.count.set(state.count.value - 1);
-      countMirror.value = state.count.value
+      state.count.set(p => p - 1);
     }
-    return { incrementCounter, decrementCounter, count, countMirror }
+    return { incrementCounter, decrementCounter, state, resetCounter}
   }
 }
 </script>
 
 <template>
-  <span class="is-size-3">
-    Counter: {{ count }} <u class="is-size-7 has-text-grey-light">(It should change just like the one below)<u /></u></span>
   <p class="is-size-3">
-    Counter Ref: {{ countMirror }}
+    Counter: {{ state.count }}
+  </p>
+  <p class="is-size-6">
+    Memory: {{ state.memory }}
   </p>
   <div class="buttons mt-3">
     <v-button type="is-danger" @click="decrementCounter">
@@ -42,6 +45,9 @@ export default {
     </v-button>
     <v-button type="is-success" @click="incrementCounter">
       +
+    </v-button>
+    <v-button type="is-info" @click="resetCounter">
+      Reset
     </v-button>
   </div>
 </template>
