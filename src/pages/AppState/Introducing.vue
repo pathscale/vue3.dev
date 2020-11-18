2f4341b095139e149f0bfc28349db76d2e39a6e6
munirapp <softwaremakassar@gmail.com>
2020-11-18 09:04:18 +0800

    Update introducing appstate docs

--- src/pages/AppState/Introducing.vue
+++ src/pages/AppState/Introducing.vue
@@ -25,6 +25,9 @@ export default {
       {{ router.currentRoute.value.meta.metaTags[0].content }}
     </p>
 
+    <br />
+    Vue has released their latest version, Vue3. Where in this latest version the <a href="https://v3.vuejs.org/api/composition-api.html" target="__blank">Composition API</a> is introduced which offers a simpler and more modern concept of state management.
+    With this new concept, you can choose more flexible state management options instead of just using Vuex. <b>Appstate-fast</b> offers better state management performance and is also highly compatible with Vue3.
 
     <snippet-section title="Global state" :code="GlobalCode" :component="Showcases" :path="router.currentRoute.value.path" />
     Create and initialize the state with the <b>createState</b> method. The returned instace could be used to get/set the state outside a Vue component.
