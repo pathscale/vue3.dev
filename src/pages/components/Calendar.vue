6ba335c9fe74da82fdfdca071a349cee17e18ea7
Oscar Albornoz <oalbornoz08@gmail.com>
2021-11-24 21:43:09 -0400

    add calendar api documentation

--- src/pages/components/Calendar.vue
+++ src/pages/components/Calendar.vue
@@ -2,7 +2,7 @@
 import { useRouter } from 'vue-router'
 import { SnippetSection, ApiSection } from '../../components'
 
-import api from '../../docs/components/api/upload.ts'
+import api from '../../docs/components/api/calendar.ts'
 
 // eslint-disable-next-line no-restricted-syntax -- using all of them
 import * as Showcases from '../../docs/components/showcases/Calendar'
